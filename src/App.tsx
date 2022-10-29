d2ec2fbe44bc93d4b91452f9360ecf0e088263b4
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-10-29 10:27:43 +0000

    add ref and new data

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,51 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ForceGraph3D } from "react-force-graph";
+import { ForceGraphMethods } from "react-force-graph-3d";
 import SpriteText from "three-spritetext";
-import links from "./edges.json";
+import edges from "./edges.json";
 import nodes from "./nodes.json";
-
 function App() {
   const [width, setWidth] = useState(0);
+
   useEffect(() => {
     if (window) setWidth(window.innerWidth);
   }, []);
+
+  const forceGraphRef = useRef<undefined | ForceGraphMethods>();
+
+  useEffect(() => {
+    if (!forceGraphRef.current) return;
+    // add gui elements
+    // const gui = forceGraphRef.current.
+  }, [forceGraphRef]);
+
   return (
     <ForceGraph3D
-      graphData={{ links, nodes }}
+      ref={forceGraphRef}
+      graphData={{ nodes, links: edges }}
       width={width}
       linkDirectionalArrowLength={3.5}
       linkDirectionalArrowRelPos={1}
       linkCurvature={0.25}
-      // linkVisibility={false}
+      nodeRelSize={8}
+      // @ts-ignore
+      nodeLabel={(node) => node.label ?? node.id}
+      // linkVisibility={(link) =>
+      //   selectedNode
+      //     ? link.source == selectedNode || link.target == selectedNode
+      //     : true
+      // }
       // nodeVisibility={(node) => node.id === 48024}
       nodeAutoColorBy="course"
-      // nodeVisibility={(node) => node.id === 48024}
+      // nodeVisibility={(node) =>
+      //   selectedNode ? node.id === selectedNode : true
+      // }
+
       enableNodeDrag={false}
       forceEngine="d3"
+      linkDirectionalParticles={1}
       nodeThreeObject={(node) => {
+        // return undefined;
         const sprite = new SpriteText(
           // @ts-ignore
           node.label
@@ -35,10 +58,6 @@ function App() {
         sprite.color = node.color;
         sprite.backgroundColor = "rgba(0, 0, 0, 0.5)";
         return sprite;
-      }}
-      onNodeClick={(node) => {
-        // @ts-ignore
-        window.open(node.url!, "_blank");
       }}
     />
   );
