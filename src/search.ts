423ff17ada038ce57a134870bd16dd206661ad9d
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-10-30 04:40:15 +0000

    idk

--- src/search.ts
+++ src/search.ts
@@ -25,37 +25,13 @@ export function dfs(
 
       if (edge.source === id) {
         dfsTravel(edge.target, depth - 1);
-      } else if (edge.target === id) {
+      } else if (edge.target === id && bidirectional) {
         dfsTravel(edge.source, depth - 1);
       }
     }
   }
 
   dfsTravel(start, maxDepth);
-  return result;
-}
-
-// breadth first search taking start and maximum depth outputting array of ids
-export function bfs(start: number): number[] {
-  const visited = new Set();
-  const result: number[] = [];
-  const queue: number[] = [start];
-
-  while (queue.length > 0) {
-    const id = queue.shift();
-    if (visited.has(id)) {
-      continue;
-    }
-    visited.add(id);
-    result.push(id!);
-    for (const edge of edges) {
-      if (edge.source === id) {
-        queue.push(edge.target);
-      } else if (edge.target === id) {
-        queue.push(edge.source);
-      }
-    }
-  }
   return result;
 }
 
@@ -82,7 +58,7 @@ export function maze(
     for (const edge of edges) {
       if (edge.source === id) {
         queue.push(edge.target);
-      } else if (edge.target === id) {
+      } else if (edge.target === id && bidirectional) {
         queue.push(edge.source);
       }
     }
