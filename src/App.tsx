9ba6d182b40a32f4260851da61f553ea68fd2011
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-10-30 06:05:39 +0000

    cool

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import SpriteText from "three-spritetext";
 import edges from "./edges.json";
 import { useWindowSize } from "./hooks";
 import nodesFromFile from "./nodes.json";
-import { dfs, maze } from "./search";
+import { relatedAndSelf } from "./search";
 const graphData = {
   nodes: nodesFromFile,
   links: edges,
@@ -31,55 +31,19 @@ interface Link {
 function App() {
   const datRef = useRef<dat.GUI | null>(null);
   const [renderSubjectId, setRenderSubjectId] = useState(48024);
-  const [renderDepth, setRenderDepth] = useState(2);
-  const [renderMazeStart, setRenderMazeStart] = useState(48024);
-  const [renderMazeEnd, setRenderMazeEnd] = useState(48024);
-  const [bidirectionalRender, setBidirectional] = useState(false);
-  const [dfsEnabled, setDfsEnabled] = useState(false);
-  const [mazeEnabled, setMazeEnabled] = useState(false);
   const { height, width } = useWindowSize();
 
   useEffect(() => {
     datRef.current = new dat.GUI();
     const gui = datRef.current;
     var params = {
-      dfsEnabled: false,
       subjectId: 48024,
-      depth: 2,
-      mazeEnabled: false,
-      mazeStart: 48024,
-      mazeEnd: 48024,
-      bidirectional: false,
     };
 
-    const dfsFolder = gui.addFolder("DFS");
-
-    const mazeFolder = gui.addFolder("Maze");
-
-    gui.add(params, "bidirectional").onFinishChange(setBidirectional);
-
-    dfsFolder.add(params, "dfsEnabled").onFinishChange(setDfsEnabled);
-
-    dfsFolder
+    gui
       .add(params, "subjectId", 0, 100000, 1)
       .name("Subject ID")
       .onFinishChange(setRenderSubjectId);
-
-    dfsFolder
-      .add(params, "depth", 0, 100, 1)
-      .name("Depth")
-      .onFinishChange(setRenderDepth);
-
-    mazeFolder.add(params, "mazeEnabled").onFinishChange(setMazeEnabled);
-
-    mazeFolder
-      .add(params, "mazeStart", 0, 100000, 1)
-      .name("Maze Start")
-      .onFinishChange(setRenderMazeStart);
-    mazeFolder
-      .add(params, "mazeEnd", 0, 100000, 1)
-      .name("Maze End")
-      .onFinishChange(setRenderMazeEnd);
 
     return () => {
       gui.destroy();
@@ -104,105 +68,53 @@ function App() {
     return populatedLinks;
   };
 
-  const getDepth = useCallback(() => {
-    const nodes = dfs(renderSubjectId, renderDepth, bidirectionalRender);
+  const getRelated = useCallback(() => {
+    const nodes = relatedAndSelf(renderSubjectId, edges);
     const links = edgesFromNodes(nodes);
     const populatedNodes = graphData.nodes.filter((node) =>
       nodes.includes(node.id)
     );
     return { nodes: populatedNodes, links };
-  }, [renderSubjectId, renderDepth, bidirectionalRender]);
-
-  const getMaze = useCallback(() => {
-    const nodes = maze(renderMazeStart, renderMazeEnd, bidirectionalRender);
-    const links = edgesFromNodes(nodes);
-
-    const populatedNodes = graphData.nodes.filter((node) =>
-      nodes.includes(node.id)
-    );
-    return { nodes: populatedNodes, links };
-  }, [renderMazeStart, renderMazeEnd, bidirectionalRender]);
+  }, [renderSubjectId, edges]);
 
   const graphFiltered = useMemo(() => {
-    if (dfsEnabled) {
-      return getDepth();
-    }
-    if (mazeEnabled) {
-      return getMaze();
-    }
-
-    return { nodes: [], links: [] };
-  }, [
-    dfsEnabled,
-    mazeEnabled,
-    renderSubjectId,
-    renderDepth,
-    renderMazeStart,
-    renderMazeEnd,
-    bidirectionalRender,
-  ]);
+    return getRelated();
+  }, [renderSubjectId, getRelated]);
 
   return (
-    <>
-      {/* <select
-        onChange={(e) => {
-          e.preventDefault();
-          setCourse(e.target.value);
-        }}
-        value={course}
-        placeholder="Select a course"
-      >
-        <option value="">All</option>
-        {[...new Set(nodesFromFile.map((node) => node.course))]
-          .filter(Boolean)
-          .map((course) => (
-            <option value={course!} key={course}>
-              {course}
-            </option>
-          ))}
-      </select>
-      <input
-        value={subjectId}
-        onChange={(e) => {
-          const parsed = parseInt(e.target.value);
-          if (!isNaN(parsed)) setSubjectId(parsed);
-        }}
-        placeholder="Subject id"
-      /> */}
-      <ForceGraph3D
-        // ref={forceGraphRef}
-        graphData={graphFiltered}
-        width={width}
-        height={height}
-        linkDirectionalArrowLength={3.5}
-        linkDirectionalArrowRelPos={1}
-        linkCurvature={0.25}
-        nodeRelSize={8}
+    <ForceGraph3D
+      // ref={forceGraphRef}
+      graphData={graphFiltered}
+      width={width}
+      height={height}
+      linkDirectionalArrowLength={3.5}
+      linkDirectionalArrowRelPos={1}
+      linkCurvature={0.25}
+      nodeRelSize={8}
+      // @ts-ignore
+      nodeLabel={(node) => node.label ?? node.id}
+      nodeAutoColorBy="course"
+      enableNodeDrag={false}
+      linkDirectionalParticles={1}
+      nodeThreeObject={(node) => {
+        const sprite = new SpriteText(
+          // @ts-ignore
+          node.label
+            ? // @ts-ignore
+              `${node.id?.toString() ?? ""}: ${node.label}`
+            : node.id?.toString()
+        );
+        sprite.textHeight = 8;
         // @ts-ignore
-        nodeLabel={(node) => node.label ?? node.id}
-        nodeAutoColorBy="course"
-        enableNodeDrag={false}
-        linkDirectionalParticles={1}
-        nodeThreeObject={(node) => {
-          const sprite = new SpriteText(
-            // @ts-ignore
-            node.label
-              ? // @ts-ignore
-                `${node.id?.toString() ?? ""}: ${node.label}`
-              : node.id?.toString()
-          );
-          sprite.textHeight = 8;
-          // @ts-ignore
-          sprite.color = node.color;
-          sprite.backgroundColor = "rgba(0, 0, 0, 0.5)";
-          return sprite;
-        }}
-        onNodeClick={(node) => {
-          // @ts-ignore
-          window.open(node.url);
-        }}
-      />
-    </>
+        sprite.color = node.color;
+        sprite.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        return sprite;
+      }}
+      onNodeClick={(node) => {
+        // @ts-ignore
+        window.open(node.url);
+      }}
+    />
   );
 }
 
--- src/search.ts
+++ src/search.ts
@@ -1,67 +1,152 @@
 import edges from "./edges.json";
 
-// depth first search taking start and depth outputting array of ids
-export function dfs(
-  start: number,
-  maxDepth: number,
-  bidirectional = false
-): number[] {
-  const visited = new Set();
-  const result: number[] = [];
+/**
+ * Structure for a node in the graph.
+ */
+interface Node {
+  id: number;
+  label: string | null;
+  url: string | null;
+  course: string | null;
+}
 
-  function dfsTravel(id: number, depth: number) {
-    if (visited.has(id)) {
-      return;
-    }
-    visited.add(id);
-    result.push(id);
-    if (depth === 0) {
-      return;
-    }
-    for (const edge of edges) {
-      if (!edge) {
-        console.log("no edge");
-      }
+/**
+ * Strucutre of the edges in the graph.
+ */
+interface DirectedEdge {
+  source: number;
+  target: number;
+}
 
-      if (edge.source === id) {
-        dfsTravel(edge.target, depth - 1);
-      } else if (edge.target === id && bidirectional) {
-        dfsTravel(edge.source, depth - 1);
-      }
+export function parents(id: number, edges: DirectedEdge[]): number[] {
+  return edges.filter((edge) => edge.target === id).map((edge) => edge.source);
+}
+
+export function ancestors(id: number, edges: DirectedEdge[]): number[] {
+  const parentsOfId = parents(id, edges);
+  return parentsOfId.concat(
+    parentsOfId.flatMap((parent) => ancestors(parent, edges))
+  );
+}
+
+export function children(id: number, edges: DirectedEdge[]): number[] {
+  return edges.filter((edge) => edge.source === id).map((edge) => edge.target);
+}
+
+export function descendants(id: number, edges: DirectedEdge[]): number[] {
+  const childrenOfId = children(id, edges);
+  return childrenOfId.concat(
+    childrenOfId.flatMap((child) => descendants(child, edges))
+  );
+}
+
+export function siblings(id: number, edges: DirectedEdge[]): number[] {
+  const parentsOfId = parents(id, edges);
+  return parentsOfId
+    .flatMap((parent) => children(parent, edges))
+    .filter((child) => child !== id);
+}
+
+export function cousins(id: number, edges: DirectedEdge[]): number[] {
+  const siblingsOfId = siblings(id, edges);
+  return siblingsOfId.flatMap((sibling) => descendants(sibling, edges));
+}
+
+export function related(id: number, edges: DirectedEdge[]): number[] {
+  return ancestors(id, edges).concat(descendants(id, edges));
+}
+
+export function relatedAndSelf(id: number, edges: DirectedEdge[]): number[] {
+  return related(id, edges).concat(id);
+}
+
+// depth first search taking start and maximum depth outputting array of ids
+export default function maze(start: number, edges: DirectedEdge[]): number[] {
+  const stack = [start];
+  const visited = new Set<number>();
+  const result = [];
+  while (stack.length > 0) {
+    const current = stack.pop();
+    if (current === undefined) {
+      continue;
     }
+    if (visited.has(current)) {
+      continue;
+    }
+    visited.add(current);
+    result.push(current);
+    stack.push(...children(current, edges));
   }
-
-  dfsTravel(start, maxDepth);
   return result;
 }
 
-// maze search taking start and end outputting array of ids
-export function maze(
-  start: number,
-  end: number,
-  bidirectional = false
-): number[] {
-  const visited = new Set();
-  const result: number[] = [];
-  const queue: number[] = [start];
-
+// breadth first search taking start and maximum depth outputting array of ids
+export function bfs(start: number, edges: DirectedEdge[]): number[] {
+  const queue = [start];
+  const visited = new Set<number>();
+  const result = [];
   while (queue.length > 0) {
-    const id = queue.shift();
+    const current = queue.shift();
+    if (current === undefined) {
+      continue;
+    }
+    if (visited.has(current)) {
+      continue;
+    }
+    visited.add(current);
+    result.push(current);
+    queue.push(...children(current, edges));
+  }
+  return result;
+}
+
+// depth first search taking start and maximum depth outputting array of ids
+export function dfs(
+  start: number,
+  edges: DirectedEdge[],
+  maxDepth: number
+): number[] {
+  const stack = [{ id: start, depth: 0 }];
+  const visited = new Set<number>();
+  const result = [];
+  while (stack.length > 0) {
+    const { id, depth } = stack.pop()!;
     if (visited.has(id)) {
       continue;
     }
     visited.add(id);
-    result.push(id!);
-    if (id === end) {
-      break;
-    }
-    for (const edge of edges) {
-      if (edge.source === id) {
-        queue.push(edge.target);
-      } else if (edge.target === id && bidirectional) {
-        queue.push(edge.source);
-      }
+    result.push(id);
+    if (depth < maxDepth) {
+      stack.push(
+        ...children(id, edges).map((child) => ({ id: child, depth: depth + 1 }))
+      );
     }
   }
   return result;
+}
+
+export function shortestPath(
+  start: number,
+  end: number,
+  edges: DirectedEdge[]
+): number[] {
+  const queue = [{ id: start, path: [start] }];
+  const visited = new Set<number>();
+  while (queue.length > 0) {
+    const { id, path } = queue.shift()!;
+    if (visited.has(id)) {
+      continue;
+    }
+    visited.add(id);
+    if (id === end) {
+      return path;
+    }
+    queue.push(
+      ...children(id, edges).map((child) => ({
+        id: child,
+        path: path.concat(child),
+      }))
+    );
+  }
+  return [];
 }
